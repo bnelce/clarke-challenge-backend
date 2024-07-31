63844d36cf0a1f5b625954d20cbba1a916ce5d26
Abner Oliveira <bnelce@yahoo.com.br>
2024-07-31 12:03:49 -0300

    removed obsoletes mutations and queries

--- app/queries/suppliers.py
+++
@@ -1,9 +0,0 @@
-from ariadne import QueryType
-from app.domain.entities.suppiler import Supplier
-
-query = QueryType()
-
-
-@query.field("suppliers")
-def resolve_suppliers(_, info, consumption):
-    return Supplier.query.filter(Supplier.min_kwh <= consumption).all()
